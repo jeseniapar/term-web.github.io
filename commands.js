938281455324af30dd37dac655cb9a52fad9524f
jeseniapar <jeseniapartha6@gmail.com>
2025-02-16 17:35:59 +0800

    commands+links

--- commands.js
+++ commands.js
@@ -1,24 +1,30 @@
+var email = "mailto:ethanj@uchicago.edu";
+var jesenia_git = "https://github.com/jeseniapar";
+var website = "https://www.ethanjiang.net/";
+var resume = "https://www.ethanjiang.net/uploads/resume.pdf"
+var cop_interview = "https://www.youtube.com/watch?v=FJKkSs3EkDY"
+var maroon_semifinals = "https://chicagomaroon.com/41007/news/uchicago-debaters-make-university-history-at-world-universities-debating-championships/"
+var iopchallenge = "https://www.instagram.com/p/C59Afs1Ohao/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA=="
+var jesenia = "https://www.instagram.com/jespar23/"
+
 const commands = {
-    about: ["I'm a fourth-year BA/MA student at the University of Chicago,",
-        "where I study Economics, International Relations, and Critical Race and Ethnic Studies.",
-        "I design policy to maximize the promise and minimize the peril of emerging technology,",
-        "with particular interests in decarbonization and AI. I've previously modeled nuclear",
-        "waste shipping networks for the federal government, managed the Dallas Mavericks' data", 
-        "warehouse and consulted on blockchain projects with Nobel Prize-winning economists."],
-    posts: ["which post...? type a key word:", "tiger", "patent", 
-        "cop28", "hotdog", "kungfu", "nationalism", "mekong", "latin", "sports", 
-        "transnationalism", "flight-tracker"],
-    projects: ["Which project...? type a key word:", "carrots", 
-        "lead-water", "green-roof", "safety", "elevator", "podcast"],
-    resume: ["i once ate many bananas"],
+    about: ["I'm a fourth-year BA/MA student at the University of Chicago, where I study Economics,", 
+        "International Relations, and Critical Race and Ethnic Studies. I design policy to maximize",
+        "the promise and minimize the peril of emerging technology, with particular interests in",
+        "decarbonization and AI. I've previously modeled nuclear waste shipping networks for the",
+        "federal government, managed the Dallas Mavericks' data warehouse and consulted on", 
+        "blockchain projects with Nobel Prize-winning economists."],
+    current: ["Thanks for asking! I'm in Chicago, finishing up my final year at UChicago. Afterwards, I will also be in Chicago for work." ],
+    news: ['Attending COP28 <a href="' + cop_interview + '" target="_blank">in Dubai' + "</a>", 
+        'Semifinalling in Vietnam <a href="' + maroon_semifinals + '" target="_blank">at the World Universities Debating Championship 2024' + "</a>", 
+        'Winning the <a href="' + iopchallenge + '" target="_blank">IOP Policy Challenge 2024' + "</a>"],
+    resume: ['Press for: <a href="' + resume + '" target="_blank">résumé' + "</a>"],
     attack: ["yes i do"],
     defense: ["i COULD get stabbed idk"],
     speed: ["faster than a lambo"],
-    type: ["meow meow meow"],
-    credits: [" -- Built by Jesenia Parthasarathy, check out: https://github.com/jeseniapar",
-        " -- Got the inspiration from: https://rona.wang/ and https://terminal.jcubic.pl/",
-        " -- Helped with Javascript logic by fkcodes: https://www.youtube.com/watch?v=KtYby2QN0kQ",
-        " -- And I learned how to build a website from this video: https://www.youtube.com/watch?v=OulQ9W2s6Z0",
-    ],
-    email: ["ethanj@uchicago.edu"]
+    type: ['<a href="' + jesenia + '" target="_blank">click to see' + "</a>"],
+    credits: ['Built by: <a href="' + jesenia_git + '" target="_blank">Jesenia:))' + "</a>"],
+    contact: ['Press to: <a href="' + email + '" target="_blank">email' + "</a>", 'Press for: <a href="' + website + '" target="_blank">official website' + "</a>"],
+    commands: ["Too lazy to scroll up and see the commands?", "about", "news", "resume", "current", "attack", "defense", "speed", "type", "credits", "contact", "commands"]
+
 };
